f97687d54811d755fb6d77211e9faaa1dd2e8659
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-01-02 20:44:07 +0700

    feat: search and paginate communities

--- app/Modules/Communities/Controllers/Api/CommunitiesController.ts
+++ app/Modules/Communities/Controllers/Api/CommunitiesController.ts
@@ -1,10 +1,15 @@
-import { HttpContext } from "@adonisjs/core/build/standalone";
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Community from "App/Modules/Communities/Community";
+import GetCommunitiesValidator from '../../Validators/GetCommunitiesValidator';
 
 export default class CommunitiesController {
 
-    public async index() {
-        return await Community.query().paginate(1, 10)
+    public async index(ctx: HttpContextContract) {
+        const query = await ctx.request.validate(GetCommunitiesValidator)
+
+        return await Community.query()
+            .whereILike('name', `%${query.search ?? ''}%`)
+            .paginate(query.page ?? 1, query.perPage ?? 10)
     }
 
 }
